3b9168ef0698494ef9c3794931533d74f322b7dd
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-10 20:04:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Write your code here
 class breakfast{
-  constructor(food, drink)
+  constructor(food, drink){
+    this.food= food
+    this.drink=drink
+  }
+}
+class lunch{
+  constructor(salad, soup, drink){
+    this.salad=salad
+    this.soup=soup
+    this.drink=drink
+  }
+}
+class dinner{
+  constructor(salad, soup, entree,  _dessert){
+    
+  }
 }
